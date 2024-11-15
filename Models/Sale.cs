3d2b9ebf047f538ee86a7bd380b65711438062c2
Alex <40648882+GhosttGtt@users.noreply.github.com>
2024-11-15 09:41:02 -0600

    Modulo de ventas y estructura creada

---
+++ Models/Sale.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace POS.Models
+{
+    public class Sale
+    {
+        public int SaleID { get; set; }
+        public int CustomerID { get; set; }
+        public int UserID { get; set; }
+        public decimal Total { get; set; }
+        public List<SaleDetail> Details { get; set; }
+    }
+
+    public class SaleDetail
+    {
+        public int SaleDetailID { get; set; }
+        public int ProductID { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace POS.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+    }
+}
